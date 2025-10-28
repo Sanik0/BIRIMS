aac13dc6f844eaf509e7bc9002161fb78b548aeb
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-10-28 14:41:40 +0800

    document page update

--- verify.php
+++ verify.php
@@ -4,18 +4,40 @@
     <body class="relative">
         <!-- Sidebar Section -->
         <?php include_once 'shared/sidebar.php' ?>
-        <!-- Main Section -->
-        <main class="w-full h-[100vh] pl-[290px] pr-[20px] flex flex-col gap-[30px] transition-all duration-300">
-            <div class="flex w-full items-start justify-between py-[25px] border-b-[1px] border-[#D4D4D8]">
+        <!-- mobile-sidebar Section -->
+        <?php include_once 'shared/mobile-sidebar.php' ?>
+        <section class="w-full items-center flex sm:hidden px-[15px] py-[10px] border-b-[1px] border-b-gray-300 justify-between">
+            <div class="flex gap-[10px] items-center">
+                <svg id="openMenuBtn" class="h-[50px] w-[50px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EFEFEF">
+                    <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                </svg>
+                <div class="h-[50px] w-[50px] rounded-[50%] bg-orange-600"></div>
+            </div>
+            <div class="flex items-center gap-[20px]">
+                <div class="h-[40px] w-[40px] rounded-[50%] border-solid border-[2px] border-[#EA580C] overflow-hidden">
+                    <img class="w-full h-full object-center object-cover" src="assets/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg" alt="">
+                </div>
+                <a href="#" class="group rounded-[4px] py-[7px] px-[8px] hover:bg-orange-700 transition-all duration-300 bg-[#EA580C] flex items-center gap-[12px]">
+                    <svg class="h-[25px] w-[25px]  group-hover:fill-gray-50 transition-all duration-300 fill-[#ffffff]" xmlns="http://www.w3.org/2000/svg" height="24px"
+                        viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
+                        <path
+                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h240q17 0 28.5 11.5T480-160q0 17-11.5 28.5T440-120H200Zm487-320H400q-17 0-28.5-11.5T360-480q0-17 11.5-28.5T400-520h287l-75-75q-11-11-11-27t11-28q11-12 28-12.5t29 11.5l143 143q12 12 12 28t-12 28L669-309q-12 12-28.5 11.5T612-310q-11-12-10.5-28.5T613-366l74-74Z" />
+                    </svg>
+                    <div class="text-[16px] group-hover:text-gray-50 transition-all duration-300 font-medium text-[#ffffff]">Log Out</div>
+                </a>
+            </div>
+        </section>
+        <main class="w-full h-[100vh] sm:pl-[290px] sm:pr-[20px] flex flex-col gap-[30px] transition-all duration-300">
+            <div class="flex w-full items-start justify-between py-[25px] border-b-[1px] border-[#D4D4D8] px-[15px] sm:px-[0]">
                 <div class="flex items-center gap-[10px] max-w-[579px]">
                     <div class="flex flex-col">
-                        <h1 class="font-serif font-medium text-[35px]">Verify Account</h1>
-                        <p class="text-[16px] font-medium text-[#A1A1AA]">Lorem ipsum dolor sit amet, consectetur adipiscing
+                        <h1 class="font-serif font-medium sm:text-[35px] text-[25px]">Verify Account</h1>
+                        <p class="sm:text-[16px] text-[14px] font-medium text-[#A1A1AA]">Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut
                              labore et dolore </p>
                     </div>
                 </div>
-                <div class="flex gap-[20px]">
+                <div class="sm:flex hidden gap-[20px]">
                     <div class="h-[50px] w-[50px] rounded-[50%] border-solid border-[2px] border-[#EA580C] overflow-hidden">
                         <img class="w-full h-full object-center object-cover" src="assets/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg" alt="">
                     </div>
@@ -30,10 +52,10 @@
                 </div>
             </div>
 
-            <section class="w-full items-center justify-center flex">
-                <!-- <div class="flex w-full max-w-[948px] flex-col gap-[30px] mb-[30px]">
-                    <div class="flex w-full gap-[30px]">
-                        <svg class="w-[290px] h-[190px]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+            <section class="w-full px-[15px] sm:px-[0] items-center justify-center flex">
+                <div class="flex w-full max-w-[948px] flex-col gap-[30px] mb-[30px]">
+                    <div class="flex flex-col sm:flex-row w-full gap-[30px]">
+                        <svg class="w-[290px] hidden sm:block h-[190px]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
                             xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
                             <path style="fill:#F2F2F2;" d="M471.917,109.027H40.084C17.981,109.027,0,127.008,0,149.111v286.463
                             c0,22.103,17.981,40.083,40.084,40.083h431.833c22.103,0,40.083-17.981,40.083-40.083V149.111
@@ -86,7 +108,7 @@
                     </div>
                     <div class="flex flex-col gap-[30px] w-full">
                         <a href="#" class="w-full flex items-center gap-[8px] rounded-[4px] border-[1px] border-gray-400 p-[10px] group">
-                            <svg class="h-[70px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <svg class="h-[70px] min-w-[50px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                                 <path d="M566.15-461.54h163.08v-40H566.15v40Zm0-110.77h163.08v-40H566.15v40ZM230.77-347.69h264.61v-6.62q0-35-35.15-54.19-35.15-19.19-97.15-19.19-62 0-97.16 19.19-35.15 19.19-35.15 54.19v6.62Zm132.31-144.62q25.3 0 42.65-17.34 17.35-17.35 17.35-42.66 0-25.31-17.35-42.65-17.35-17.35-42.65-17.35-25.31 0-42.66 17.35-17.34 17.34-17.34 42.65t17.34 42.66q17.35 17.34 42.66 17.34ZM184.62-200q-27.62 0-46.12-18.5Q120-237 120-264.62v-430.76q0-27.62 18.5-46.12Q157-760 184.62-760h590.76q27.62 0 46.12 18.5Q840-723 840-695.38v430.76q0 27.62-18.5 46.12Q803-200 775.38-200H184.62Zm0-40h590.76q9.24 0 16.93-7.69 7.69-7.69 7.69-16.93v-430.76q0-9.24-7.69-16.93-7.69-7.69-16.93-7.69H184.62q-9.24 0-16.93 7.69-7.69 7.69-7.69 16.93v430.76q0 9.24 7.69 16.93 7.69 7.69 16.93 7.69ZM160-240v-480 480Z" />
                             </svg>
                             <div class="flex flex-col">
@@ -97,7 +119,7 @@
                     </div>
                     <div class="flex flex-col gap-[30px] w-full">
                         <a href="#" class="w-full flex items-center gap-[8px] rounded-[4px] border-[1px] border-gray-400 p-[10px] group">
-                            <svg class="h-[70px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <svg class="h-[70px] min-w-[50px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                                 <path d="M566.15-461.54h163.08v-40H566.15v40Zm0-110.77h163.08v-40H566.15v40ZM230.77-347.69h264.61v-6.62q0-35-35.15-54.19-35.15-19.19-97.15-19.19-62 0-97.16 19.19-35.15 19.19-35.15 54.19v6.62Zm132.31-144.62q25.3 0 42.65-17.34 17.35-17.35 17.35-42.66 0-25.31-17.35-42.65-17.35-17.35-42.65-17.35-25.31 0-42.66 17.35-17.34 17.34-17.34 42.65t17.34 42.66q17.35 17.34 42.66 17.34ZM184.62-200q-27.62 0-46.12-18.5Q120-237 120-264.62v-430.76q0-27.62 18.5-46.12Q157-760 184.62-760h590.76q27.62 0 46.12 18.5Q840-723 840-695.38v430.76q0 27.62-18.5 46.12Q803-200 775.38-200H184.62Zm0-40h590.76q9.24 0 16.93-7.69 7.69-7.69 7.69-16.93v-430.76q0-9.24-7.69-16.93-7.69-7.69-16.93-7.69H184.62q-9.24 0-16.93 7.69-7.69 7.69-7.69 16.93v430.76q0 9.24 7.69 16.93 7.69 7.69 16.93 7.69ZM160-240v-480 480Z" />
                             </svg>
                             <div class="flex flex-col">
@@ -108,7 +130,7 @@
                     </div>
                     <div class="flex flex-col gap-[30px] w-full">
                         <a href="#" class="w-full flex items-center gap-[8px] rounded-[4px] border-[1px] border-gray-400 p-[10px] group">
-                            <svg class="h-[70px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <svg class="h-[70px] min-w-[50px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                                 <path d="M566.15-461.54h163.08v-40H566.15v40Zm0-110.77h163.08v-40H566.15v40ZM230.77-347.69h264.61v-6.62q0-35-35.15-54.19-35.15-19.19-97.15-19.19-62 0-97.16 19.19-35.15 19.19-35.15 54.19v6.62Zm132.31-144.62q25.3 0 42.65-17.34 17.35-17.35 17.35-42.66 0-25.31-17.35-42.65-17.35-17.35-42.65-17.35-25.31 0-42.66 17.35-17.34 17.34-17.34 42.65t17.34 42.66q17.35 17.34 42.66 17.34ZM184.62-200q-27.62 0-46.12-18.5Q120-237 120-264.62v-430.76q0-27.62 18.5-46.12Q157-760 184.62-760h590.76q27.62 0 46.12 18.5Q840-723 840-695.38v430.76q0 27.62-18.5 46.12Q803-200 775.38-200H184.62Zm0-40h590.76q9.24 0 16.93-7.69 7.69-7.69 7.69-16.93v-430.76q0-9.24-7.69-16.93-7.69-7.69-16.93-7.69H184.62q-9.24 0-16.93 7.69-7.69 7.69-7.69 16.93v430.76q0 9.24 7.69 16.93 7.69 7.69 16.93 7.69ZM160-240v-480 480Z" />
                             </svg>
                             <div class="flex flex-col">
@@ -119,7 +141,7 @@
                     </div>
                     <div class="flex flex-col gap-[30px] w-full">
                         <a href="#" class="w-full flex items-center gap-[8px] rounded-[4px] border-[1px] border-gray-400 p-[10px] group">
-                            <svg class="h-[70px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <svg class="h-[70px] min-w-[50px] w-[70px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                                 <path d="M566.15-461.54h163.08v-40H566.15v40Zm0-110.77h163.08v-40H566.15v40ZM230.77-347.69h264.61v-6.62q0-35-35.15-54.19-35.15-19.19-97.15-19.19-62 0-97.16 19.19-35.15 19.19-35.15 54.19v6.62Zm132.31-144.62q25.3 0 42.65-17.34 17.35-17.35 17.35-42.66 0-25.31-17.35-42.65-17.35-17.35-42.65-17.35-25.31 0-42.66 17.35-17.34 17.34-17.34 42.65t17.34 42.66q17.35 17.34 42.66 17.34ZM184.62-200q-27.62 0-46.12-18.5Q120-237 120-264.62v-430.76q0-27.62 18.5-46.12Q157-760 184.62-760h590.76q27.62 0 46.12 18.5Q840-723 840-695.38v430.76q0 27.62-18.5 46.12Q803-200 775.38-200H184.62Zm0-40h590.76q9.24 0 16.93-7.69 7.69-7.69 7.69-16.93v-430.76q0-9.24-7.69-16.93-7.69-7.69-16.93-7.69H184.62q-9.24 0-16.93 7.69-7.69 7.69-7.69 16.93v430.76q0 9.24 7.69 16.93 7.69 7.69 16.93 7.69ZM160-240v-480 480Z" />
                             </svg>
                             <div class="flex flex-col">
@@ -128,7 +150,7 @@
                             </div>
                         </a>
                     </div>
-                </div> -->
+                </div>
 
                 <!-- <div class="w-full flex max-w-[696px] flex-col gap-[50px]">
                     <div class="flex flex-col gap-[20px]">
@@ -160,14 +182,15 @@
                         </div>
                     </form>
                 </div> -->
-                <div class=" max-w-[792px] flex flex-col gap-[20px] rounded-[4px] border-solid border-[1px] border-gray-300 p-[30px]">
+                
+                <!-- <div class=" max-w-[792px] flex flex-col gap-[20px] rounded-[4px] border-solid border-[1px] border-gray-300 p-[30px]">
                     <h1 class="text-[40px] font-bold">Verification Pending</h1>
                     <p class="text-[20px] text-gray-500 font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  labore et dolore </p>
-                    <div class="w-full max-w-[481px] h-[287px] overflow-hidden rounded-[4px]">
+                    <div class="w-full max-w-[481px] h-fit overflow-hidden rounded-[4px]">
                         <img class="w-full h-full object-center onbject-cover" src="assets/Screenshot 2025-10-01 204358.png" alt="">
                     </div>
                     <a class="w-full flex items-center justify-center px-[20px] py-[10px] text-[20px] text-[#FDBA74] font-medium rounded-[4px] border-[1px] border-[#FDBA74] hover:bg-orange-100 hover:text-orange-700 transition-all duration-300 hover:cursor-pointer" href="notifications.php">View Notifications</a>
-                </div>
+                </div> -->
             </section>
         </main>
 
