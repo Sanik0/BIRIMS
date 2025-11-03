701c700b3238ba2370fdefd2fb3a0a2203200f40
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-03 14:21:46 +0800

    orders fixed images link

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -27,7 +27,7 @@
                 </div>
                 <div class="flex items-center gap-[20px]">
                     <div class="h-[40px] w-[40px] rounded-[50%] border-solid border-[2px] border-[#EA580C] overflow-hidden">
-                        <img class="w-full h-full object-center object-cover" src="assets/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg" alt="">
+                        <img class="w-full h-full object-center object-cover" src="{{asset('images/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg')}}" alt="">
                     </div>
                     <a href="#" class="group rounded-[4px] py-[7px] px-[8px] hover:bg-orange-700 transition-all duration-300 bg-[#EA580C] flex items-center gap-[12px]">
                         <svg class="h-[25px] w-[25px]  group-hover:fill-gray-50 transition-all duration-300 fill-[#ffffff]" xmlns="http://www.w3.org/2000/svg" height="24px"
@@ -36,7 +36,7 @@
                                 d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h240q17 0 28.5 11.5T480-160q0 17-11.5 28.5T440-120H200Zm487-320H400q-17 0-28.5-11.5T360-480q0-17 11.5-28.5T400-520h287l-75-75q-11-11-11-27t11-28q11-12 28-12.5t29 11.5l143 143q12 12 12 28t-12 28L669-309q-12 12-28.5 11.5T612-310q-11-12-10.5-28.5T613-366l74-74Z" />
                         </svg>
                         <div class="text-[16px] group-hover:text-gray-50 transition-all duration-300 font-medium text-[#ffffff]">Log Out</div>
-                    </a>
+                    </a>    
                 </div>
             </section>
             <main class="w-full h-[100vh] sm:pl-[290px] sm:pr-[20px] flex flex-col gap-[30px] transition-all duration-300">
@@ -51,7 +51,7 @@
                     </div>
                     <div class="sm:flex hidden gap-[20px]">
                         <div class="h-[50px] w-[50px] rounded-[50%] border-solid border-[2px] border-[#EA580C] overflow-hidden">
-                            <img class="w-full h-full object-center object-cover" src="assets/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg" alt="">
+                            <img class="w-full h-full object-center object-cover" src="{{asset('images/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg')}}" alt="">
                         </div>
                         <a href="#" class="group rounded-[4px] hover:bg-orange-700 transition-all duration-300 bg-[#EA580C] px-[20px] flex items-center gap-[12px]">
                             <svg class="h-[25px] w-[25px]  group-hover:fill-gray-50 transition-all duration-300 fill-[#ffffff]" xmlns="http://www.w3.org/2000/svg" height="24px"
@@ -68,7 +68,7 @@
                     <div class="flex items-start px-[15px] gap-[30px] sm:gap-[0] sm:px-[0] sm:items-center justify-between flex-col sm:flex-row w-full py-[30px] border-b-gray-300 border-b-[1px]">
                         <div class="flex gap-[15px]">
                             <div class="overflow-hidden border-[1px] border-gray-200 rounded-[4px]">
-                                <img class="object-center object-cover h-[138px] w-[96px]" src="assets/indegency.png" alt="">
+                                <img class="object-center object-cover h-[138px] w-[96px]" src="{{asset('images/indegency.png')}}" alt="">
                             </div>
                             <div class="flex flex-col py-[10px] max-w-[225px]">
                                 <h3 class="text-[20px] font-semibold">Barangay Certificate of Residency</h3>
@@ -88,7 +88,7 @@
                     <div class="flex items-start px-[15px] gap-[30px] sm:gap-[0] sm:px-[0] sm:items-center justify-between flex-col sm:flex-row w-full py-[30px] border-b-gray-300 border-b-[1px]">
                         <div class="flex gap-[15px]">
                             <div class="overflow-hidden border-[1px] border-gray-200 rounded-[4px]">
-                                <img class="object-center object-cover h-[138px] w-[96px]" src="assets/indegency.png" alt="">
+                                <img class="object-center object-cover h-[138px] w-[96px]" src="{{asset('images/indegency.png')}}" alt="">
                             </div>
                             <div class="flex flex-col py-[10px] max-w-[225px]">
                                 <h3 class="text-[20px] font-semibold">Barangay Certificate of Residency</h3>
@@ -108,7 +108,7 @@
                     <div class="flex items-start px-[15px] gap-[30px] sm:gap-[0] sm:px-[0] sm:items-center justify-between flex-col sm:flex-row w-full py-[30px] border-b-gray-300 border-b-[1px]">
                         <div class="flex gap-[15px]">
                             <div class="overflow-hidden border-[1px] border-gray-200 rounded-[4px]">
-                                <img class="object-center object-cover h-[138px] w-[96px]" src="assets/indegency.png" alt="">
+                                <img class="object-center object-cover h-[138px] w-[96px]" src="{{asset('images/indegency.png')}}" alt="">
                             </div>
                             <div class="flex flex-col py-[10px] max-w-[225px]">
                                 <h3 class="text-[20px] font-semibold">Barangay Certificate of Residency</h3>
@@ -128,7 +128,7 @@
                     <div class="flex items-start px-[15px] gap-[30px] sm:gap-[0] sm:px-[0] sm:items-center justify-between flex-col sm:flex-row w-full py-[30px] border-b-gray-300 border-b-[1px]">
                         <div class="flex gap-[15px]">
                             <div class="overflow-hidden border-[1px] border-gray-200 rounded-[4px]">
-                                <img class="object-center object-cover h-[138px] w-[96px]" src="assets/indegency.png" alt="">
+                                <img class="object-center object-cover h-[138px] w-[96px]" src="{{asset('images/indegency.png')}}" alt="">
                             </div>
                             <div class="flex flex-col py-[10px] max-w-[225px]">
                                 <h3 class="text-[20px] font-semibold">Barangay Certificate of Residency</h3>
