93698584ac7d2a9aebb8a6d72baa2ac53e97dc6f
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-06 23:33:10 +0800

    minor fix in sidebar active page indicator

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -19,13 +19,13 @@
           <div class="w-full flex flex-col">
               <a href="{{ url('/home')}}" class="group flex gap-[10px] items-center py-[10px]">
                   <svg class="h-[30px] w-[30px] 
-                    {{ request()->is('home') ? 'fill-[#ea580c]' : 'group-hover:fill-[#71717A] fill-[#A1A1AA]'}} transition-all duration-300 " xmlns="http://www.w3.org/2000/svg" height="24px"
+                    {{ request()->is('home') || request()->is('/') ? 'fill-[#ea580c]' : 'group-hover:fill-[#71717A] fill-[#A1A1AA]'}} transition-all duration-300 " xmlns="http://www.w3.org/2000/svg" height="24px"
                       viewBox="0 -960 960 960" width="24px" fill="#EFEFEF">
                       <path
                           d="M160-200v-360q0-19 8.5-36t23.5-28l240-180q21-16 48-16t48 16l240 180q15 11 23.5 28t8.5 36v360q0 33-23.5 56.5T720-120H600q-17 0-28.5-11.5T560-160v-200q0-17-11.5-28.5T520-400h-80q-17 0-28.5 11.5T400-360v200q0 17-11.5 28.5T360-120H240q-33 0-56.5-23.5T160-200Z" />
                   </svg>
                   <div class="font-semibold text-[20px]
-                    {{request()->is('home') ? 'text-[#ea580c]' : 'group-hover:text-[#71717A] text-[#A1A1AA]'}} transition-all duration-300">Home</div>
+                    {{request()->is('home') || request()->is('/') ? 'text-[#ea580c]' : 'group-hover:text-[#71717A] text-[#A1A1AA]'}} transition-all duration-300">Home</div>
               </a>
               <a href="{{ url('/notifications')}}" class="group flex gap-[10px] items-center py-[10px]">
                   <svg class="h-[30px] w-[30px] 
