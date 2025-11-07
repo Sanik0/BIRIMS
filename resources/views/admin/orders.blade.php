b618381ab784dabcab4dec451e3cc7dcb34e3b4a
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-07 10:18:21 +0800

    improved filtering UI

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -188,7 +188,7 @@
                                       <path d="M520-600v-80h120v-160h80v160h120v80H520Zm120 480v-400h80v400h-80Zm-400 0v-160H120v-80h320v80H320v160h-80Zm0-320v-400h80v400h-80Z" />
                                   </svg>
                                   <select class="appearance-none focus:outline-none" name="" id="">
-                                      <option value="Filter">Filter</option>
+                                      <option value="Filter">Filter By Status:</option>
                                   </select>
                               </div>
 
