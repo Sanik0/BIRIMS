e637009faff2a977a91af80d10b93836445af687
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-07 11:03:14 +0800

    fixed edit modal for blotter ereports

--- resources/views/admin/blotters.blade.php
+++ resources/views/admin/blotters.blade.php
@@ -35,17 +35,82 @@
        </form>
    </div>
    <!-- modal edit announcement Section -->
-   <div id="editModal" class="w-full modal fixed inset-0 overflow-y-auto p-[15px] sm:p-[50px] bg-black/50 backdrop-blur-[5px] z-[999] hidden items-center justify-center">
+   <div id="editModal" class="modal w-full fixed inset-0 overflow-y-auto p-[15px] sm:p-[50px] bg-black/50 backdrop-blur-[5px] z-[999] hidden justify-center">
        <form class="rounded-[4px] h-fit bg-white p-[15px] sm:p-[30px] flex flex-col w-full max-w-[540px] gap-[30px]">
-           <h3 class="font-bold text-[40px]">User Verified</h3>
-           <div class="flex items-center justify-center w-full">
-               <p class="font-regular w-full text-[20px] text-gray-500">User <span class="text-[#EF4444]"> Juan Dela Cruz</span> Has been verified.</p>
+           <h3 class="font-bold text-[40px]">Edit User</h3>
+           <div class="flex flex-col sm:flex-row items-center gap-[30px] w-full">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">First Name:</Label>
+                   <input type="text" placeholder="Ex. Juan" value="Juan" class="py-[10px] border-b-[1px]  border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Last Name:</Label>
+                   <input type="text" placeholder="Ex. Dela Cruz" value="Dela Cruz" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col gap-[10px]">
+               <div class="flex flex-col">
+                   <Label class="font-medium text-[18px]">Middle Name:</Label>
+                   <input type="text" placeholder="(Optional)" value="Santiago" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col sm:flex-row items-center gap-[30px] w-full">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Date of Incident:</Label>
+                   <input type="date" value="2005-10-09" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col gap-[10px]">
+               <div class="flex flex-col">
+                   <Label class="font-medium text-[18px]">Name of Respondent:</Label>
+                   <input type="Email" value="Pedro Santos" placeholder="Ex. Pedro Santos" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col sm:flex-row items-center gap-[30px] w-full">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Contact of Respondent:</Label>
+                   <input type="text" value="0912345678" placeholder="" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Age of Respondent:</Label>
+                   <input type="number" value="34" placeholder="" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col gap-[10px]">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Address of Respondent:</Label>
+                   <textarea type="text" placeholder="address" value="" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">123 Mabini St. San Isidro Quezon city Metro Manila</textarea>
+               </div>
+           </div>
+           <div class="flex flex-col sm:flex-row items-center gap-[30px] w-full">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Complaint:</Label>
+                   <input type="text" value="Physical assault during argument" placeholder="Ex. Teacher" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+           </div>
+           <div class="flex flex-col gap-[10px]">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Description of incident:</Label>
+                   <textarea type="text" placeholder="address" value="" class="py-[10px] min-h-[300px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</textarea>
+               </div>
+           </div>
+           <div class="flex flex-col sm:flex-row items-center gap-[30px] w-full">
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Date of Incident:</Label>
+                   <input type="date" placeholder="" value="2025-01-05" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
+               <div class="flex flex-col w-full">
+                   <Label class="font-medium text-[18px]">Time of incident:</Label>
+                   <input type="time" placeholder="" value="13:45" class="py-[10px] border-b-[1px] border-b-gray-700 focus:outline-none font-regular text-gray-600 text-[18px]">
+               </div>
            </div>
            <div class="flex flex-col w-full gap-[20px]">
-               <button class="w-full cancelBtn flex items-center justify-center px-[20px] py-[10px] text-[20px] bg-[#EA580C] text-[#ffffff] font-medium rounded-[4px] border-[1px] border-[#EA580C] hover:bg-orange-700 transition-all duration-300 hover:cursor-pointer">Go Back</button>
+               <button class="w-full flex items-center justify-center px-[20px] py-[10px] text-[20px] bg-[#EA580C] text-[#ffffff] font-medium rounded-[4px] border-[1px] border-[#EA580C] hover:bg-orange-700 transition-all duration-300 hover:cursor-pointer">Edit Report</button>
+               <div id="" class="cancelBtn flex items-center justify-center px-[20px] py-[10px] text-[20px] text-[#FDBA74] font-medium rounded-[4px] border-[1px] border-[#FDBA74] hover:bg-orange-100 hover:text-orange-700 transition-all duration-300 hover:cursor-pointer">Cancel</div>
            </div>
        </form>
    </div>
+
 
    <body class="relative">
        <!--sidebar section -->
@@ -81,7 +146,7 @@
                        <img class="h-full w-full object-center object-cover" src="{{asset('images/3d cartoon avatar of a man minimal 3d character _ Premium AI-generated image.jpg')}}" alt="">
                    </div>
                    <div class="flex flex-col">
-                       <h1 class="font-serif font-medium sm:text-[35px] text-[25px]">Manage Verifications</h1>
+                       <h1 class="font-serif font-medium sm:text-[35px] text-[25px]">Manage Blotter Reports</h1>
                        <p class="sm:text-[16px] text-[14px] font-medium text-[#A1A1AA]">Lorem ipsum dolor sit amet, consectetur adipiscing
                            elit, sed do eiusmod tempor incididunt ut
                             labore et dolore </p>
@@ -110,7 +175,7 @@
                                <path d="M520-600v-80h120v-160h80v160h120v80H520Zm120 480v-400h80v400h-80Zm-400 0v-160H120v-80h320v80H320v160h-80Zm0-320v-400h80v400h-80Z" />
                            </svg>
                            <select class="appearance-none focus:outline-none" name="" id="">
-                               <option value="Filter">Filter By Document:</option>
+                               <option value="Filter">Filter By Street:</option>
                            </select>
                        </div>
 
@@ -126,31 +191,31 @@
                <table class="w-full hidden sm:table border-collapse text-left border-[1px] border-gray-300 rounded-[4px]">
                    <thead>
                        <tr class="border-b-[1px] border-gray-300 bg-[#FFF7ED]">
-                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Full Name</th>
-                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Email</th>
-                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Document</th>
-                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Date Submitted</th>
+                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Complainant</th>
+                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Respondent</th>
+                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Complaint</th>
+                           <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Date of incident</th>
                            <th class="px-[20px] py-[10px] font-medium text-[16px] text-gray-600">Action</th>
                        </tr>
                    </thead>
                    <tbody>
                        <tr class="border-b-[1px] border-gray-300 bg-white">
                            <td data-modal="addModal" class="addBtn hover:underline cursor-pointer px-[20px] py-[10px] font-regular text-[16px] text-black">Juan Dela Cruz</td>
-                           <td class="px-[20px] py-[10px] font-regular text-[16px] text-gray-600">juandelacruz@gmail.com</td>
-                           <td class="px-[20px] py-[10px] font-regular text-[16px] text-gray-600">Valid ID</td>
+                           <td class="px-[20px] py-[10px] font-regular text-[16px] text-gray-600">Pedro Santos</td>
+                           <td class="px-[20px] py-[10px] font-regular text-[16px] text-gray-600">Physical assault during argument</td>
                            <td class="px-[20px] py-[10px] font-regular text-[16px] text-gray-600">03-05-2025</td>
                            <td class="px-[20px] py-[10px] font-regular text-[16px] w-fit text-gray-600 flex items-center gap-[10px]">
                                <div data-modal="editModal" class="editBtn hover:bg-green-100 hover:text-green-500 hover:border-green-500 group cursor-pointer transition-all duration-300 rounded-[4px] px-[10px] py-[3px] flex items-center gap-[8px] border-[1px] border-gray-400 font-medium text-[14px] text-gray-400">
                                    <svg class="h-[20px] transition-all duration-300 group-hover:fill-green-500 w-[20px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
-                                       <path d="m424-408-86-86q-11-11-28-11t-28 11q-11 11-11 28t11 28l114 114q12 12 28 12t28-12l226-226q11-11 11-28t-11-28q-11-11-28-11t-28 11L424-408Zm56 328q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                                       <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                                    </svg>
-                                   Verify
+                                   Edit
                                </div>
                                <div data-modal="deleteModal" class="deleteBtn hover:bg-red-100 hover:text-red-500 hover:border-red-500 group cursor-pointer transition-all duration-300 rounded-[4px] px-[10px] py-[3px] flex items-center gap-[8px] border-[1px] border-gray-400 font-medium text-[14px] text-gray-400">
                                    <svg class="h-[20px] transition-all duration-300 group-hover:fill-red-500 w-[20px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
-                                       <path d="m480-424 116 116q11 11 28 11t28-11q11-11 11-28t-11-28L536-480l116-116q11-11 11-28t-11-28q-11-11-28-11t-28 11L480-536 364-652q-11-11-28-11t-28 11q-11 11-11 28t11 28l116 116-116 116q-11 11-11 28t11 28q11 11 28 11t28-11l116-116Zm0 344q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                                       <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                                    </svg>
-                                   Reject
+                                   Delete
                                </div>
                            </td>
                        </tr>
@@ -172,15 +237,15 @@
                        <div class="w-full flex items-center gap-[10px]">
                            <div data-modal="editModal" class="editBtn hover:bg-green-100 hover:text-green-500 hover:border-green-500 group cursor-pointer transition-all duration-300 rounded-[4px] px-[10px] py-[3px] flex items-center gap-[8px] border-[1px] border-gray-400 font-medium text-[14px] text-gray-400">
                                <svg class="h-[20px] transition-all duration-300 group-hover:fill-green-500 w-[20px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
-                                   <path d="m424-408-86-86q-11-11-28-11t-28 11q-11 11-11 28t11 28l114 114q12 12 28 12t28-12l226-226q11-11 11-28t-11-28q-11-11-28-11t-28 11L424-408Zm56 328q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                                   <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
                                </svg>
-                               Verify
+                               Edit
                            </div>
                            <div data-modal="deleteModal" class="deleteBtn hover:bg-red-100 hover:text-red-500 hover:border-red-500 group cursor-pointer transition-all duration-300 rounded-[4px] px-[10px] py-[3px] flex items-center gap-[8px] border-[1px] border-gray-400 font-medium text-[14px] text-gray-400">
                                <svg class="h-[20px] transition-all duration-300 group-hover:fill-red-500 w-[20px] fill-gray-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
-                                   <path d="m480-424 116 116q11 11 28 11t28-11q11-11 11-28t-11-28L536-480l116-116q11-11 11-28t-11-28q-11-11-28-11t-28 11L480-536 364-652q-11-11-28-11t-28 11q-11 11-11 28t11 28l116 116-116 116q-11 11-11 28t11 28q11 11 28 11t28-11l116-116Zm0 344q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                                   <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                                </svg>
-                               Reject
+                               Delete
                            </div>
                        </div>
                    </div>
