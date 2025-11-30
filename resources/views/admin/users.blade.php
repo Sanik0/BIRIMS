2d70810a711be15dbd7326dbaad64d87d0151cbd
Sanik0 <sanico.ronan.macugay@gmail.com>
2025-11-30 10:46:10 +0800

    minor fixess on the alert modal

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -214,6 +214,15 @@
      </div>
 
      <body class="relative">
+         <!-- Alerts Modal -->
+         <div class="absolute w-full py-[20px] flex items-center justify-center">
+             <div class="flex items-start sm:items-center bg-[#e1ffe7] p-4 mb-4 text-sm text-medium rounded-[8px] border border-[rgb(40,194,71)]" role="alert">
+                 <svg class="w-4 h-4 me-2 shrink-0 mt-0.5 sm:mt-0 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                     <path class="stroke-[rgb(40,194,71)]" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                 </svg>
+                 <p class="text-[rgb(40,194,71)]"><span class="font-medium me-1 text-[rgb(40,194,71)]">Info alert!</span> Change a few things up and try submitting again.</p>
+             </div>
+         </div>
          <!--sidebar section -->
          @include('admin.shared.sidebar')
          <!-- mobile sidebar section -->
@@ -241,12 +250,6 @@
              </div>
          </section>
          <main class="w-full h-[100vh] sm:pl-[290px] sm:pr-[20px] flex flex-col gap-[30px] transition-all duration-300">
-             <div class="flex items-start sm:items-center bg-[rgba(0,255,51,0.1)] p-4 mb-4 text-sm text-medium rounded-[8px] border border-[rgb(40,194,71)]" role="alert">
-                 <svg class="w-4 h-4 me-2 shrink-0 mt-0.5 sm:mt-0 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                     <path class="stroke-[rgb(40,194,71)]" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                 </svg>
-                 <p><span class="font-medium me-1 text-[rgb(40,194,71)]">Info alert!</span> Change a few things up and try submitting again.</p>
-             </div>
              <div class="flex w-full items-start justify-between py-[25px] border-b-[1px] border-[#D4D4D8] px-[15px] sm:px-[0]">
                  <div class="flex items-center gap-[10px] max-w-[579px]">
                      <div class="h-[90px] min-w-[90px] sm:flex hidden max-w-[90px] rounded-[50%] overflow-hidden">
